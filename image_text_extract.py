4262a9e7dbbfeebed6da8e293eadb25617aa2998
Jayesh Poonia <jp0102@srmist.edu.in>
2024-03-14 07:38:37 +0530

    working img->text upload

---
+++ image_text_extract.py
@@ -0,0 +1,18 @@
+from PIL import Image 
+from pytesseract import pytesseract 
+# Defining paths to tesseract.exe  
+# and the image we would be using 
+path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+image_path = r"D:\Github_App\Online_Video_KYC\DataBase_For_ID_Card\10004.jpeg"
+# Opening the image & storing it in an image object 
+img = Image.open(image_path) 
+# Providing the tesseract  
+# executable location to pytesseract library 
+pytesseract.tesseract_cmd = path_to_tesseract 
+# Passing the image object to  
+# image_to_string() function 
+# This function will 
+# extract the text from the image 
+text = pytesseract.image_to_string(img) 
+# Displaying the extracted text 
+print(text[:-1])
